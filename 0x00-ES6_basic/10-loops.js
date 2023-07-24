554ced75fa45b13298b7ecea9ae0dd4a815ebbf7
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-07-24 14:24:11 +0000

    js

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const newArray = [];
+    for (const idx of array) {
+      newArray.push(`${appendString}${array[idx]}`);
+    }
+  
+    return newArray;
+  }
+  
